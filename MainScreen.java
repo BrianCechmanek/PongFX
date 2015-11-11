4101a2f7f7993a3e05662910997064d0622c3087
BrianCechmanek <bcechmanek@gmail.com>
2015-11-10 19:04:11 -0700

    Keyevent first attempt

--- MainScreen.java
+++ MainScreen.java
@@ -5,8 +5,9 @@ import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
 import javafx.stage.*;
 import javafx.scene.*;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 
@@ -38,29 +39,29 @@ public class MainScreen extends Application {
 		playerPaddle = new Paddle(canvas.getWidth()/15, canvas.getHeight()/2.5 );
 		aiPaddle = new Paddle( (canvas.getWidth()*.93), canvas.getHeight()/2.5 );
 		ball = new Ball(canvas.getHeight());
-
-		
-		//set up a timeline and key listener so the paddles can move 
-		// (and later the ball and score et c)
-//		mainScene.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
-
-//			@Override
-//			public void handle(KeyEvent event) {
-				
-				
-//			}
-			
-			
-//		});
-		
-		
-
 		
 		//Do the drawing
 		canvas.getChildren().addAll(playerPaddle, aiPaddle, ball);
 		//start up the level
 		level = 1;
 		ball.move(ball);
+
+
+		mainScene.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+				if ((event.getCode() == KeyCode.KP_UP) && (playerPaddle.getLayoutY() > 0))  {
+					System.out.println("KeyCode is" + event.getCode());
+					Paddle.moveUp(playerPaddle);
+				}
+				else if ((event.getCode() == KeyCode.KP_DOWN) && (playerPaddle.getLayoutY() < MainScreen.canvas.getHeight()))  {
+					System.out.println("KeyCode is" + event.getCode());
+					Paddle.moveDown(playerPaddle);
+				} else {
+					System.out.println("KeyCode is " + event.getCode()  + " but if/else isn't catching codes correctly.");
+				}
+		    }		
+		});			
 		mainStage.setScene(mainScene);
 		mainStage.show();
 	}
--- Paddle.java
+++ Paddle.java
@@ -1,17 +1,20 @@
 package pong;
 
+import java.awt.event.KeyListener;
+
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
 import javafx.util.Duration;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.animation.*;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.geometry.Bounds;
 
 public class Paddle extends Rectangle{
+	
 	
 	public Paddle(double i, double d) {
 		
@@ -22,16 +25,78 @@ public class Paddle extends Rectangle{
 		this.setHeight(MainScreen.canvas.getHeight()/6);
 		this.setWidth(MainScreen.canvas.getWidth()/50);
 		this.setFill(Color.ALICEBLUE);
-		
-		
+	}
+	
+	public static void moveUp(Rectangle paddle) {
+		paddle.setLayoutY(paddle.getLayoutY() - 20);
+	}
+	
+	public static void moveDown(Rectangle paddle) {
+		paddle.setLayoutY(paddle.getLayoutY() +  20);
 	}
 
-	public void moveUp() {
-		this.setLayoutY(this.getLayoutY() - 5);
-	}
 	
-	public void moveDown() {
-		this.setLayoutY(this.getLayoutY() +  5);
-	}
-	
+} //close class paddle
+
+/*
+MainScreen.canvas.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+	@Override
+	public void handle(KeyEvent event) {
+		if ((event.getCode() == KeyCode.KP_UP) && (paddle.getLayoutY() > 0))  {
+			System.out.println("KeyCode is" + event.getCode());
+			moveUp(paddle);
+		}
+		else if ((event.getCode() == KeyCode.KP_DOWN) && (paddle.getLayoutY() < MainScreen.canvas.getHeight()))  {
+			System.out.println("KeyCode is" + event.getCode());
+			moveDown(paddle);
+		} else {
+			System.out.println("KeyCode is" + event.getCode()  + " but it isn't registering correctly.");
+		}
+	}));
+
+
+public void move(Rectangle paddle) {
+	final Timeline loop = new Timeline(new KeyFrame(Duration.millis(10), new EventHandler<KeyEvent>() {
+			        MainScreen.canvas.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler <KeyEvent>() {
+			        	    @Override
+				        	    public void handle(KeyEvent event) {
+				        		if ((event.getCode() == KeyCode.KP_UP) && (paddle.getLayoutY() > 0))  {
+				        			System.out.println("KeyCode is" + event.getCode());
+				        			moveUp(paddle);
+				        		}
+				        		else if ((event.getCode() == KeyCode.KP_DOWN) && (paddle.getLayoutY() < MainScreen.canvas.getHeight()))  {
+				        			System.out.println("KeyCode is" + event.getCode());
+				        			moveDown(paddle);
+				        		} else {
+				        			System.out.println("KeyCode is" + event.getCode()  + " but it isn't registering correctly.");
+				        		}			        	     	
+				        }
+			    }
+             )));
+
+    loop.setCycleCount(Timeline.INDEFINITE);
+    loop.play();
 }
+*/
+
+/*	public void move(Paddle paddle) {
+ final Timeline loop = new Timeline(new KeyFrame(Duration.millis(10), new EventHandler<KeyEvent>() {
+	@Override
+	public void handle(KeyEvent event) {
+		if ((event.getCode() == KeyCode.KP_UP) && (paddle.getLayoutY() > 0))  {
+			System.out.println("KeyCode is" + event.getCode());
+			moveUp(paddle);
+		}
+		else if ((event.getCode() == KeyCode.KP_DOWN) && (paddle.getLayoutY() < MainScreen.canvas.getHeight()))  {
+			System.out.println("KeyCode is" + event.getCode());
+			moveDown(paddle);
+		} else {
+			System.out.println("KeyCode is" + event.getCode()  + " but it isn't registering correctly.");
+		}
+	}
+	}));
+loop.setCycleCount(Timeline.INDEFINITE);
+loop.play();	
+
+} // close move()
+*/
